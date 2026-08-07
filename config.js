8f8d22f0026ff28dce3c2c84d28682602d077d67
lachezar-todorov <lachezar1976@gmail.com>
2026-08-07 11:44:50 +0300

    Restored config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Постави тук линка от Apps Script (Deploy → Web app), завършващ на /exec
-const API_URL = 'https://script.google.com/macros/s/AKfycbwr2lc95Dk76jcULHULUb76NHZHiRBZph9iO9pS-me3P3lbWaOR4VXbrg1IFSgdO4rBxg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxRA-iXnyFab8BeiiRMsyAu2K0Su5nQGh4EOwur8GiSkKyaXsSE8eMB_kImCxAYkbG6dA/exec';
 
